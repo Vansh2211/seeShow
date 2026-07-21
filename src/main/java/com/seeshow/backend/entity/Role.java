b96bbbaeace09e48bff609c5f3bf90479259c491
Vansh <mehtavansh2211@gmail.com>
2026-07-21 12:01:12 +0530

    User and role entites

---
+++ src/main/java/com/seeshow/backend/entity/Role.java
@@ -0,0 +1,19 @@
+package com.seeshow.backend.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Role extends BaseEntity {
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+
+    @Column(nullable = false, length = 255)
+    private String description;
+
+}
