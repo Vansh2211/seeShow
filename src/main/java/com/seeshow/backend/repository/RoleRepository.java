98e1bd20a626b777dce3de4ab0c5fb334dba5c64
Vansh <mehtavansh2211@gmail.com>
2026-07-24 17:50:33 +0530

    Initial repositories created

--- src/main/java/com/seeshow/backend/repository/RoleRepository.java
+++ src/main/java/com/seeshow/backend/repository/RoleRepository.java
@@ -1,5 +1,11 @@
 package com.seeshow.backend.repository;
 
-public class RoleRepository {
+import com.seeshow.backend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.*;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
--- src/main/java/com/seeshow/backend/repository/UserRepository.java
+++ src/main/java/com/seeshow/backend/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.seeshow.backend.repository;
 
-public class UserRepository {
+import com.seeshow.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.*;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
